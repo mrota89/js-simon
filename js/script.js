48165c7f9c4bd456e1e4ff0f06d56b281ee12754
mrota89 <marcello.rota.1989@gmail.com>
2021-01-15 17:24:43 +0100

    esercizio base completato

--- js/script.js
+++ js/script.js
@@ -4,38 +4,49 @@ function randomNumber(min, max) {
   return result;
 }
 
-//dichiaro variabile per array numeri casuali
+//dichiaro variabili
 var numeriDaIndovinare = 5;
-var numeriGenerati = [];
+var timing = 30;
 var numeroUtente;
+var numeriGenerati = [];
+var numeriGeneratiAlert = [];
 var numeriInseriti = [];
 var numeriIndovinati = [];
+var numeriIndovinatiAlert = [];
 
 //genero numeri casuali
 for(var i = 0; i < numeriDaIndovinare; i++) {
-  numeriGenerati.push(numeroCasuale = randomNumber(1, 1000));
+  numeriGenerati.push(randomNumber(1, 1000));
+  numeriGeneratiAlert.push('  ' + numeriGenerati[i]);
 }
 
-console.log(numeriGenerati);
+console.log('numeri generati:' + numeriGenerati);
 
 //mostro all'utente i numeri generati
-alert('Benvenuto in una nuova partita' + '\nOra ti verranno mostrati 5 numeri. Memorizzali!')
-alert(numeriGenerati);
-alert('Tra 30 secondi ti verrà chiesto di inserire i numeri che hai memorizzato' + '\nPremi ok per avviare il timer.')
+alert('Benvenuto in una nuova partita!' + '\nOra ti verranno mostrati ' + numeriDaIndovinare + ' numeri. Memorizzali!' + '\nPoi attendi ' + timing + ' secondi.');
+alert(numeriGeneratiAlert + '\nPremi Invio quando sei pronto.');
 
-//imposto callback che sarà richiamata dopo 30 secondi
+//imposto callback anonima che sarà richiamata dopo 30 secondi dalla chiusura dell'alert.
 setTimeout(function(){
-  while(numeriInseriti.length < numeriDaIndovinare) {
+  while(numeriInseriti.length < numeriDaIndovinare) {//finchè non saranno inseriti 5 numeri, resto nel ciclo while
     numeroUtente = parseInt(prompt('inserisci un numero'));
     if(isNaN(numeroUtente)) {
-      alert('inserisci un numero!')
+      alert('inserisci un numero!');
     } else {
       numeriInseriti.push(numeroUtente);
       if(numeriGenerati.includes(numeroUtente)) {
         numeriIndovinati.push(numeroUtente);
+        numeriIndovinatiAlert.push('  ' + numeroUtente);
       }
     }
   }
-  alert('Hai indovinato questi numeri: ' + numeriIndovinati + ' \nPer un totale di: ' + numeriIndovinati.length)
-  console.log(numeriInseriti);
-}, 30000);
+
+  console.log('numeri inseriti: ' + numeriInseriti);
+  console.log('numeri indovinati: ' + numeriIndovinati);
+
+  if(numeriIndovinati.length === 0) {
+    alert('Hai Perso! Nessun numero indovinato.');
+  } else {
+    alert('Hai indovinato questi numeri: ' + numeriIndovinatiAlert + ' \nPer un totale di: ' + numeriIndovinati.length);
+  }
+}, timing * 1000);
